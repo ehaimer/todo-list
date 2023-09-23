6caafca200d5ee09a4ca8bbc4c7fb648e82fa7b3
ehaimer <y.ehaimer@gmail.com>
2023-09-23 02:39:17 -0400

    Modify CSS

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   return (
     <main className="flex flex-col h-screen container border mx-auto px-6 py-12 gap-y-8 my-16 rounded-lg drop-shadow-xl bg-white">
       <section className="flex flex-col gap-y-8">
-        <h1 className="text-primary font-bold text-3xl">My To-Do List</h1>
+        <h1 className="text-primary font-bold text-3xl whitespace-nowrap">My To-Do List</h1>
         <form
           onSubmit={handleSubmit}
           className="flex flex-col gap-y-6 text-gray-500"
@@ -67,7 +67,7 @@ function App() {
             type="text"
             ref={todoRef}
             placeholder="Enter your To Do Item"
-            className="rounded-full py-2 px-4 drop-shadow-md focus:outline-none"
+            className="rounded-full py-2 px-4 drop-shadow-md focus:outline-none min-w-[220px]"
           />
           <div className="flex gap-x-4">
             <Button
@@ -83,7 +83,7 @@ function App() {
         </form>
       </section>
       <section className="ml-4 flex flex-col gap-y-4">
-        <h2 className="text-lg font-bold text-primary">
+        <h2 className="text-lg font-bold text-primary whitespace-nowrap">
           {todoList.filter((todo) => !todo.isComplete).length} Left Todo
         </h2>
         <TodoList
